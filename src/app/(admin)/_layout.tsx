f06773db996030bd94802c1d90472dbd49d9d658
KhadzA <alimkhadz111@gmail.com>
2024-09-21 18:14:40 +0800

    UI finished

--- src/app/(admin)/_layout.tsx
+++ src/app/(admin)/_layout.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable } from 'react-native';
+import { Link, Tabs, } from 'expo-router';
+import { Platform, Pressable } from 'react-native';
 import Colors from '@/constants/Colors';
 import { useColorScheme } from '@/components/useColorScheme';
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
+import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
@@ -16,37 +17,47 @@ function TabBarIcon(props: {
 
 export default function TabLayout() {
   const colorScheme = useColorScheme() ?? 'light';
+  const insets = useSafeAreaInsets();
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors.light.background,
-        tabBarInactiveTintColor: 'gainsboro',
-        tabBarStyle: {
-          backgroundColor: Colors.light.tint,
-        },
-      }}>
-      
-      <Tabs.Screen name="index" options={{ href: null }} />
+    <SafeAreaView
+      edges={['bottom']}
+      style={{
+        flex: 1,
+        backgroundColor: Colors[colorScheme].background,
+        paddingBottom: Platform.OS === 'android' ? insets.bottom : 0,
+      }}
+    >
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: Colors.light.background,
+          tabBarInactiveTintColor: 'gainsboro',
+          tabBarStyle: {
+            backgroundColor: Colors.light.tint,
+          },
+        }}>
+        
+        <Tabs.Screen name="index" options={{ href: null }} />
 
-      <Tabs.Screen
-        name="menu"
-        options={{
-          title: 'Menu',
-          headerShown: false,
-          tabBarIcon: ({ color }) => (
-            <TabBarIcon name="cutlery" color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="orders"
-        options={{
-          title: 'Orders',
-          headerShown: false,
-          tabBarIcon: ({ color }) => <TabBarIcon name="list" color={color} />,
-        }}
-      />
-    </Tabs>
+        <Tabs.Screen
+          name="menu"
+          options={{
+            title: 'Menu',
+            headerShown: false,
+            tabBarIcon: ({ color }) => (
+              <TabBarIcon name="cutlery" color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="orders"
+          options={{
+            title: 'Orders',
+            headerShown: false,
+            tabBarIcon: ({ color }) => <TabBarIcon name="list" color={color} />,
+          }}
+        />
+      </Tabs>
+    </SafeAreaView>
   );
 }
